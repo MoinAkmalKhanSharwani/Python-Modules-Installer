df85c4bf23552b92208bf26d72e8dc047694c9cc
MoinAkmalKhanSharwani <42810023+MoinAkmalKhanSharwani@users.noreply.github.com>
2018-09-07 23:55:40 +0500

    Update Quick-Modules-Installer.py

--- Quick-Modules-Installer.py
+++ Quick-Modules-Installer.py
@@ -30,6 +30,7 @@ try:
         mod = input("Enter the Name of Python Module to install : ")
         if mod:
             if mod.lower() == "exit":
+                print("\nReload this program to install new Modules of Python.")
                 sys.exit()
             else:
                 veri = os.listdir("C:\\Users\\{}\\AppData\\Local\\Programs\\Python".format(user))
@@ -71,25 +72,22 @@ try:
                         sys.exit()
                 elif len(veri)>2:
                     def versioninfo():
-                        print("You have many folder or versions of python Please Select your python folder or latest version")
+                        print("You have many versions of python. Please Select your python folder.")
                         for i in range(0,len(veri)):
                             pyveri = veri[i]
                             print("({})".format(i), pyveri)
-                        print("You have many folder or versions of python. \nPlease Select your python folder or latest version")
+                        print("You have many versions of python. Please Select your python folder.")
                         while True:
                             try:
-                                foldernum=int(input("Type the Number of your python folder which how in display : "))
+                                foldernum=int(input("Type the Number of your python folder which show in display : "))
                                 if foldernum:
                                     if foldernum > i:
-                                        print("Error: {} not found \nThe number you type is too high and not found in the list of folders.".format(foldernum))
+                                        print("Error: {} not found. \nThe number you type is too high and not found in the list of folders.".format(foldernum))
                                         continue
                                     else:
                                         break
-                                else:
-                                    print("\n\n\n\nError: Please type number of Python folder")
-                                    versioninfo()
                             except ValueError:
-                                print("ValueError: The Folder Number must be a number")  
+                                print("ValueError: The Folder Number must be a number. Please type a number.")  
                                 continue                      
                         gen = veri[foldernum]
                         return gen
@@ -98,9 +96,8 @@ try:
                     more = input("Do You want to install more ? [Yes/No] : ")
                     if more.lower()=="yes" or  more.lower()=="y":
                         lable()
-                        continue
                     else:
-                        break
+                        print("{} ThanksYou for using.......".format(user))
                         sys.exit()
                 else:
                     x = str(*veri)
@@ -108,12 +105,147 @@ try:
                     more = input("Do You want to install more ? [Yes/No] : ")
                     if more.lower()=="yes" or  more.lower()=="y":
                         lable()
-                        continue
                     else:
-                        break
+                        print("{} ThanksYou for using .......".format(user))
                         sys.exit()
         else:
-            print("Please Type Module Name or type Exit to exit")
+            print("Please Type Module Name or type Exit to exit.")
             continue
 except Exception as e:
     print("Error : ",e,"\nPlease Try again")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# file = open("C:\\Users\\MAKS\\AppData\\Local\\Google\\Chrome\\User Data\\Default\\Login Data","r")
+# print(file.read())
+# file.close()
+
+# import pyttsx
+# e = pyttsx.init()
+# e.say("Hello world")
+# e.runAndWait()
+
+
+# import warnings as pd
+# x=dir(pd)
+# for i in x:
+#     print(i)
+
+# import matplotlib.pyplot as plt
+# fig=plt.figure("Histrogram")
+# ax = fig.add_subplot(1,1,1)
+# ax.hist([45,87,12,34,123,512,21,541,541],bins=7,facecolor="r",normed=True)
+# plt.title("Distription")
+# plt.xlabel("Range")
+# plt.ylabel("Amount")
+# plt.show()
+
+# x=dir(plt)
+# for i in x:
+#     print(i)
+
+
+
+# import getpass# a=getpass.getpass("Enter ")
+# u = getpass.getuser()
+
+# print(a)
+# print(u)
+
+# l = [1,2,3,4,5,6,7,8,9,10]
+# print(list(filter(lambda a: a%2 == 0 ,l)))
+
+# l = ["moin", "akmal", "khan", "sharwani"]
+# print(max(l, key = lambda item: len(item)))
+
+# import webbrowser
+# # a = dir(webbrowser)
+# # for i in a:
+# #     print(i)
+# print(webbrowser.__doc__)
+
+# a = {1,2,3,4,5,6,7,8,9,10}
+# b= {1,2,3,4,6,7,8,9}
+# print(a^b)#intersection
+# print(a|b)#union
+# print(a-b)#difference
+
+# import sys
+# fonct = dir(sys)
+# print(sys.path)
+# # for i in fonct:
+# #     print(i)
+
+
+# def name(a):
+#     if type(a) != type("a"):
+#         assert a == 3, (f"{a} = 3")
+#         assert a>3, (f"{a} > 3")
+#         assert a<3, (f"{a} < 3")
+
+#     else:
+#         print(f"Your string is {a.title()}")
+# try:
+#     name(3)
+# except ValueError as e:
+#     print(e)
+# except AssertionError as e:
+#     print(e)
+
+
+# li=[]
+# l = 0
+# while "stop" not in li or "no" not in li:
+#     if "stop" in li or "no" in li:
+#         li.pop()
+#         break
+#     else:
+#         print("\nNote :: Type no or stop to stop this")
+#         l = input("Enter a item of list : ")
+#         li.append(l)
+# for i in range(0,len(li)):
+#     rev = li[i][::-1]
+#     print(rev)
+
+# l = []
+# for i in range(1,11):
+#     l.append(i)
+# print(l) 
+# l = [i*i for i in range(1,11)]
+# print(l)
